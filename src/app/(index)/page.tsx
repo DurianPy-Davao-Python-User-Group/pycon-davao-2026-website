0da396fb2ad9d94924bc9ee24507aa4cfd36d975
maksdeb_g <mxdave02@gmail.com>
2026-08-19 03:52:31 +0800

    feat: add responsive (mobile/tablet) Conference at-a-glance section

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero';
+import Conference from '@/components/Conference';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Conference />
     </>
   );
 }
