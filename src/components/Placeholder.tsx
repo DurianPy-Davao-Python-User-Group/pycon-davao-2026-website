e045029807c62e8ef2cdc9d84b8689c8ac06308a
maksdeb_g <mxdave02@gmail.com>
2026-08-20 03:40:05 +0800

    style: tidy placeholder section class order

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -3,7 +3,7 @@ import mobileBg from '@/assets/conference/mobile-bg.svg';
 
 export default function Placeholder() {
   return (
-    <section className="relative flex min-h-[100dvh] w-full flex-col justify-between overflow-hidden px-2">
+    <section className="relative flex min-h-[100dvh] w-full flex-col justify-between overflow-hidden bg-black px-2">
       <div>Hello</div>
     </section>
   );
