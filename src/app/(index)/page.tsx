5633e2cc8a63e35ef4d23806976a5b78ca7ad85c
Ricky Rodrigo <rickyrodrigo.ph@gmail.com>
2026-08-20 18:18:26 +0800

    feat: add PyCon Davao 2025 highlights section

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero';
+import PyCon2025Highlights from '@/components/PyCon2025Highlights';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <PyCon2025Highlights />
     </>
   );
 }
