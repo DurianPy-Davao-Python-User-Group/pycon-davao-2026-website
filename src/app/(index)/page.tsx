95870ca892292142b10c6681d2ec5475d4f1b819
maksdeb_g <mxdave02@gmail.com>
2026-08-22 04:34:19 +0800

    feat: add cta section in page index

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero';
 import Conference from '@/components/Conference';
+import CallToAction from '@/components/CallToAction';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Conference />
+      <CallToAction />
     </>
   );
 }
