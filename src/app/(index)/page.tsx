d3af29b1b6c19019ece08c85a3e3c64670ca9a8d
Hanz Regalado <derrenhanzregalado@gmail.com>
2026-08-22 20:07:37 +0800

    feat: add home faqs

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,11 +1,14 @@
 import Hero from '@/components/Hero';
 import Conference from '@/components/Conference';
+import HomeFaqSection from '@/components/sections/home-faq-section';
+import { HomeFaqSectionData } from '@/data/home-faq-section-data';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Conference />
+      <HomeFaqSection title="Frequently Asked Questions" data={HomeFaqSectionData} />
     </>
   );
 }
