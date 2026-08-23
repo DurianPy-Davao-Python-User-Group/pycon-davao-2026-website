0f58bfb026621402ffcc57382309300e250eefb8
seangaaab <seangabbayron@gmail.com>
2026-08-23 21:35:03 +0800

    feat: extend PyCon Davao 2025 highlights
    Co-authored-by: Ricky Rodrigo <rickyrodrigo.ph@gmail.com>

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero';
 import Conference from '@/components/Conference';
+import PyCon2025Highlights from '@/components/PyCon2025Highlights';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Conference />
+      <PyCon2025Highlights />
     </>
   );
 }
