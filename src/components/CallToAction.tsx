97226a7f887d923c8911257e2e4158ffeec856a8
maksdeb_g <mxdave02@gmail.com>
2026-08-23 14:38:35 +0800

    feat: replace cta tail assets with new svgs and shape-tracing drop shadows

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 import ctaBg from '@/assets/cta/mobile-bridge-bg.svg';
 import ctaBridgePattern from '@/assets/cta/cta-bridge-pattern.svg';
 import ctaCard from '@/assets/cta/cta-image.jpg';
-import tailLeft from '@/assets/cta/tail-left.svg';
-import tailRight from '@/assets/cta/tail-right.svg';
+import tailLeft from '@/assets/cta/left-tail.svg';
+import tailRight from '@/assets/cta/right-tail.svg';
 import { Button } from '@/components/ui/button';
 
 export default function CallToAction() {
   return (
-    <section className="to-pycon-teal relative flex h-auto min-h-[65dvh] w-full flex-col overflow-hidden bg-gradient-to-b from-transparent p-6 sm:p-10 md:min-h-[70dvh] md:p-14 lg:min-h-[80dvh] xl:min-h-[105dvh] lg:p-20">
+    <section className="to-pycon-teal relative flex h-auto min-h-[65dvh] w-full flex-col overflow-hidden bg-gradient-to-b from-transparent p-6 sm:p-10 md:min-h-[70dvh] md:p-14 lg:min-h-[80dvh] lg:p-20 xl:min-h-[115dvh]">
       {/* Mobile background */}
       <Image
         src={ctaBg}
@@ -48,10 +48,10 @@ export default function CallToAction() {
           <h3 className="font-heading text-secondary mt-1 text-xl font-bold sm:text-2xl md:text-3xl lg:text-4xl">
             Ready to level up your Python journey?
           </h3>
-          <p className="text-foreground w-auto text-center text-sm sm:text-base md:text-lg lg:text-xl">
+          <p className="text-foreground w-auto text-center text-sm sm:text-base md:text-lg lg:w-[550px] lg:text-xl xl:w-auto">
             Secure your badge and be part of Mindanao&apos;s biggest open-source celebration.
           </p>
-          <Button className="cursor-pointer w-[200px] md:w-[300px] px-6 py-2.5 text-sm sm:px-8 sm:py-3 sm:text-base md:px-10 md:py-3.5 md:text-lg lg:text-xl font-bold rounded-full">
+          <Button className="w-[200px] cursor-pointer rounded-full px-6 py-2.5 text-sm font-bold sm:px-8 sm:py-3 sm:text-base md:w-[300px] md:px-10 md:py-3.5 md:text-lg lg:text-xl">
             Get Tickets
           </Button>
         </div>
@@ -65,7 +65,7 @@ export default function CallToAction() {
         unoptimized
         width={371}
         height={741}
-        className="pointer-events-none absolute -left-1 bottom-58 z-40 w-[37vw] sm:bottom-55 sm:w-[30vw] md:bottom-68 md:w-[28vw] lg:bottom-70 lg:w-[30vw] xl:bottom-47 xl:w-[28vw]"
+        className="pointer-events-none absolute bottom-58 -left-32 z-40 w-[70vw] drop-shadow-[0px_4px_39.3px_#00000040] md:bottom-78 md:-left-45 md:w-[50vw] lg:bottom-80 lg:-left-80 lg:w-[60vw] xl:bottom-95 xl:w-[45vw]"
       />
 
       <Image
@@ -75,7 +75,7 @@ export default function CallToAction() {
         unoptimized
         width={311}
         height={411}
-        className="pointer-events-none absolute -right-1 bottom-0 z-40 h-auto w-[30vw] sm:w-[25vw] md:w-[20vw] lg:-bottom-10 lg:w-[28vw] xl:-bottom-20 xl:w-[25vw]"
+        className="pointer-events-none absolute -right-22 -bottom-4 z-40 w-[48vw] drop-shadow-[0px_4px_39.3px_#00000040] md:w-[35vw] lg:-right-30 lg:-bottom-18 lg:w-[40vw] xl:-bottom-20 xl:w-[30vw]"
       />
     </section>
   );
