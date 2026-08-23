2b184c2295639874a3e3ce7cb7eb61f387cd628f
jl-hurdman <leonard@hurdman.net>
2026-08-23 21:15:22 +0800

    feat(what-is-pycon): what is pycon section of the homepage

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,5 +1,6 @@
 import Hero from '@/components/Hero';
 import Conference from '@/components/Conference';
+import WhatIsPyCon from './components/what-is-pycon';
 import HomeFaqSection from '@/components/sections/home-faq-section';
 import { HomeFaqSectionData } from '@/data/home-faq-section-data';
 import PyCon2025Highlights from '@/components/PyCon2025Highlights';
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <WhatIsPyCon />
       <Conference />
       <PyCon2025Highlights />
       <HomeFaqSection data={HomeFaqSectionData} />
